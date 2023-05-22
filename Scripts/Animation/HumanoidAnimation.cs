9ccacaf5c2d1fcf29a9396d762a2ee993ce501d6
Michal <malpa.w.klapkach@gmail.com>
2023-05-23 00:26:16 +0200

    Handle unsed animator parameters

--- Scripts/Animation/HumanoidAnimation.cs
+++ Scripts/Animation/HumanoidAnimation.cs
@@ -14,10 +14,33 @@ namespace Bipolar.Humanoid3D.Animation
             animator = GetComponentInChildren<Animator>();
         }
 
-        protected void SetTrigger(int hash) => animator.SetTrigger(hash);
-        protected void ResetTrigger(int hash) => animator.ResetTrigger(hash);
-        protected void SetBool(int hash, bool value) => animator.SetBool(hash, value);
-        protected void SetInteger(int hash, int value) => animator.SetInteger(hash, value);
-        protected void SetFloat(int hash, float value) => animator.SetFloat(hash, value);
+        protected void SetTrigger(int hash)
+        {
+            if (hash != 0)
+                animator.SetTrigger(hash);
+        }
+
+        protected void ResetTrigger(int hash)
+        {
+            if (hash != 0)
+                animator.ResetTrigger(hash);
+        }
+
+        protected void SetBool(int hash, bool value)
+        {
+            if (hash != 0)
+                animator.SetBool(hash, value);
+        }
+        protected void SetInteger(int hash, int value)
+        {
+            if (hash != 0)
+                animator.SetInteger(hash, value);
+        }
+
+        protected void SetFloat(int hash, float value)
+        {
+            if (hash != 0)
+                animator.SetFloat(hash, value);
+        }
     }
 }
