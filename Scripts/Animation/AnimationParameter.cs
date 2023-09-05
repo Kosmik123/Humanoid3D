138b71cd03e47d5a0be1ebbb2e76272874b88e84
Kosmik <malpa.w.klapkach@gmail.com>
2023-09-05 10:33:25 +0200

    Animation Parameter refactoring

--- Scripts/Animation/AnimationParameter.cs
+++ Scripts/Animation/AnimationParameter.cs
@@ -1,5 +1,4 @@
 ﻿using UnityEngine;
-using UnityEditor;
 
 namespace Bipolar.Humanoid3D.Animation
 {
@@ -10,34 +9,26 @@ namespace Bipolar.Humanoid3D.Animation
         private string name;
         [SerializeField]
         private int hash;
+        private bool hasValue;
 
-        public int Value => hash;
+        public int Value
+        {
+            get
+            {
+                if (hasValue == false)
+                {
+                    hash = Animator.StringToHash(name);
+                    hasValue = true;
+                }
+                return hash;
+            }
+        }
 
         public AnimationParameter(string name)
         {
             this.name = name;
             hash = Animator.StringToHash(name);
-        }
-    }
-
-    [CustomPropertyDrawer(typeof(AnimationParameter))]
-    public class AnimationParameterDrawer : PropertyDrawer
-    {
-        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
-        {
-            EditorGUI.BeginProperty(position, label, property);
-
-            float labelWidth = 0.43f;
-            var labelRect = position;
-            labelRect.width *= labelWidth;
-            EditorGUI.PrefixLabel(labelRect, label);
-
-            var fieldRect = position;
-            fieldRect.width *= (1 - labelWidth);
-            fieldRect.x += labelRect.width;
-            EditorGUI.TextField(fieldRect, property.FindPropertyRelative("name").stringValue);
-
-            EditorGUI.EndProperty();
+            hasValue = true;
         }
     }
 }
---
+++ Scripts/Editor/AnimationParameterDrawer.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace Bipolar.Humanoid3D.Animation
+{
+#if UNITY_EDITOR
+    [CustomPropertyDrawer(typeof(AnimationParameter))]
+    public class AnimationParameterDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            EditorGUI.BeginProperty(position, label, property);
+
+            float labelWidth = 0.43f;
+            var labelRect = position;
+            labelRect.width *= labelWidth;
+            EditorGUI.PrefixLabel(labelRect, label);
+
+            var fieldRect = position;
+            fieldRect.width *= (1 - labelWidth);
+            fieldRect.x += labelRect.width;
+            EditorGUI.TextField(fieldRect, property.FindPropertyRelative("name").stringValue);
+
+            EditorGUI.EndProperty();
+        }
+    }
+#endif
+}
