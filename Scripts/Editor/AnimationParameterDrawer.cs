670e5428bbb394850c05f850d5c406665cc26faf
Michal <malpa.w.klapkach@gmail.com>
2023-10-18 12:05:10 +0200

    Remove GIT conflict comments

--- Scripts/Editor/AnimationParameterDrawer.cs
+++ Scripts/Editor/AnimationParameterDrawer.cs
@@ -2,27 +2,6 @@
 
 namespace Bipolar.Humanoid3D.Animation
 {
-<<<<<<<< HEAD:Scripts/Editor/AnimationParameterDrawer.cs
-#if UNITY_EDITOR
-    [CustomPropertyDrawer(typeof(AnimationParameter))]
-    public class AnimationParameterDrawer : PropertyDrawer
-    {
-        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
-        {
-            EditorGUI.BeginProperty(position, label, property);
-
-            float labelWidth = 0.43f;
-            var labelRect = position;
-            labelRect.width *= labelWidth;
-            EditorGUI.PrefixLabel(labelRect, label);
-
-            var fieldRect = position;
-            fieldRect.width *= (1 - labelWidth);
-            fieldRect.x += labelRect.width;
-            EditorGUI.TextField(fieldRect, property.FindPropertyRelative("name").stringValue);
-
-            EditorGUI.EndProperty();
-========
     [System.Serializable]
     public struct AnimationParameter
     {
@@ -50,8 +29,6 @@ namespace Bipolar.Humanoid3D.Animation
             this.name = name;
             hash = Animator.StringToHash(name);
             hasValue = true;
->>>>>>>> 8e2cf2edbcfb14e9128bd393a1facebfe24ade42:Scripts/Animation/AnimationParameter.cs
         }
     }
-#endif
 }
