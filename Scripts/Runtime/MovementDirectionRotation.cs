596ee767d878a876eebe237ffb85978a195ad805
Kosmik <malpa.w.klapkach@gmail.com>
2024-06-11 08:14:00 +0200

    Remove Angle struct

--- Scripts/Runtime/MovementDirectionRotation.cs
+++ Scripts/Runtime/MovementDirectionRotation.cs
@@ -1,4 +1,3 @@
-using Bipolar;
 using Bipolar.Humanoid3D;
 using UnityEngine;
 
@@ -10,12 +9,12 @@ public class MovementDirectionRotation : MonoBehaviour
     [SerializeField]
     private float rotationSpeed = 200;
 
-    private Angle rotationAngle;
+    private float rotationAngle;
     private float targetRotationAngle;
 
     private void OnEnable()
     {
-        rotationAngle = Angle.FromDegrees(humanoid.transform.rotation.eulerAngles.y);
+        rotationAngle = humanoid.transform.rotation.eulerAngles.y;
     }
 
     private void Update()
@@ -28,7 +27,7 @@ public class MovementDirectionRotation : MonoBehaviour
         velocity.y = 0;
         targetRotationAngle = Vector3.SignedAngle(Vector3.forward, velocity, Vector3.up);
 
-        rotationAngle = Angle.FromDegrees(Mathf.MoveTowardsAngle(rotationAngle.ToDegrees(), targetRotationAngle, rotationSpeed * Time.deltaTime));
-        humanoidTransform.rotation = Quaternion.AngleAxis(rotationAngle.ToDegrees(), Vector3.up);
+        rotationAngle = Mathf.MoveTowardsAngle(rotationAngle, targetRotationAngle, rotationSpeed * Time.deltaTime);
+        humanoidTransform.rotation = Quaternion.AngleAxis(rotationAngle, Vector3.up);
     }
 }
