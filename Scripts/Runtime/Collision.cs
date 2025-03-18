4b79d313e5f67b4e074356b7e88cfd5c7f342dcc
Kosmik <malpa.w.klapkach@gmail.com>
2025-03-18 18:45:41 +0100

    Remove HumanoidController

---
+++ Scripts/Runtime/Collision.cs
@@ -0,0 +1,11 @@
+﻿namespace Bipolar.Humanoid3D
+{
+	[System.Flags]
+    public enum Collision
+    {
+        None = 0,
+        Sides = 1 << 0,
+        Above = 1 << 1,
+        Below = 1 << 2,
+    }
+}
---
+++ Scripts/Runtime/HumanoidUpdater.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Bipolar.Humanoid3D
+{
+	public class HumanoidUpdater : MonoBehaviour
+    {
+        [SerializeField]
+        private HumanoidComponent[] components;
+        public IReadOnlyList<HumanoidComponent> Components => components;
+    }
+}
